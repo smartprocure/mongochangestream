fe227b061f35f8bddd940ed101538c1ffc2c9fee
David Sargeant <david@dsargeant.com>
2023-11-20 15:42:46 -0500

    Add another test.

--- src/util.test.ts
+++ src/util.test.ts
@@ -2,7 +2,21 @@ import { test } from 'node:test'
 import assert from 'node:assert'
 import { generatePipelineFromOmit } from './util.js'
 
-test('should generate pipeline from omit', async () => {
+test('should generate pipeline from omit with no dotted fields', async () => {
+  const pipeline = generatePipelineFromOmit(['documents', 'createdAt'])
+  assert.deepEqual(pipeline, [
+    {
+      $unset: [
+        'fullDocument.documents',
+        'updateDescription.updatedFields.documents',
+        'fullDocument.createdAt',
+        'updateDescription.updatedFields.createdAt',
+      ],
+    },
+  ])
+})
+
+test('should generate pipeline from omit with dotted fields', async () => {
   const pipeline = generatePipelineFromOmit([
     'documents.agenda.parsedText',
     'documents.agenda.contentType',
